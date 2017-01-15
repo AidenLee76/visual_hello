0a289f32d71ecc5bebdb6daf7384e49a3096b27a
AidenLee76 <masklee76@gmail.com>
2017-01-15 21:48:58 +0900

    Program.cs modified on GitHub

--- Program.cs
+++ Program.cs
@@ -12,7 +12,8 @@ namespace HelloWorld
         {
             Console.WriteLine("Hello World");
             Console.WriteLine("World is mine");
-            Console.WriteLine("Luna is mine");
+            Console.WriteLine("Luna is mine");            
+            Console.WriteLine("World on GitHub");
         }
     }
 }
